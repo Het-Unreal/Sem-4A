692d86032fba64eb991f5bc76bc663860baa543f
Het Kathiriya <kathiriyahet4@gmail.com>
2024-02-29 11:42:25 +0530

    Update prac04.java

--- Java/prac04.java
+++ Java/prac04.java
@@ -25,8 +25,7 @@ public class prac04{
         for(int i=0;i<3;i++){
             for(int j=0;j<3;j++){
                 arrsum[i][j]=arr1[i][j]+arr2[i][j];
-            }
-            System.out.println();
+            }
         }
         for(int i=0;i<3;i++){
             for(int j=0;j<3;j++){
